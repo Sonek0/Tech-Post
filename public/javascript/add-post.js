955aed4d8bfe4c635ad6068c125e79de9ae40844
jacob <jacob.7050@gmail.com>
2023-03-30 22:42:52 -0700

    javascript for login/post/delete have been  functions esablished.

---
+++ public/javascript/add-post.js
@@ -0,0 +1,27 @@
+//"async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style, avoiding the need to explicitly configure promise chains."
+async function newFormHandler(event) {
+  event.preventDefault();
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_content = document
+    .querySelector('textarea[name="post-content"]')
+    .value.trim();
+    //"removes whitespace from both ends of a string" 
+  const response = await fetch(`/api/posts`, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      post_content,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+}
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
---
+++ public/javascript/comment.js
@@ -0,0 +1,32 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    if (comment_text) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                comment_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+//if else true false boolean "if true return... if false return..."
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            //https://developer.mozilla.org/en-US/docs/Web/API/Response/status (resource)
+            //status read-only property of the Response interface contains the status code of the response 
+            alert(response.statusText);
+        }
+    }
+}
+//listen for the submit button 0
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
