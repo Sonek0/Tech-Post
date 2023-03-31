10210d989e7c16b0256e01e3fcbbf947d83c5dd3
jacob <jacob.7050@gmail.com>
2023-03-30 21:18:22 -0700

    apis were established and the controllers

---
+++ server.js
@@ -0,0 +1,55 @@
+// Dependencies
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+const routes = require("./controllers/");
+const helpers = require("./utils/helpers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: process.env.DB_SESSION_SECRET,
+  cookie: {
+    // maxAge:
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+const hbs = exphbs.create({
+  helpers,
+});
+app.engine("handlebars", hbs.engine);
+
+//app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
+//path to the routes
+app.use(routes);
+
+sequelize
+  .sync({
+    force: false,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  });
